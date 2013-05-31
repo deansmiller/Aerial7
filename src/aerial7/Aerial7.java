0d83e677f8981ff80b29b40d4b9f43fb45df9983
Dean S. Miller <dean.sylvester.miller@gmail.com>
2013-05-31 16:35:25 +0100

    Minor changes. NEEDS refactoring, get it working first then get it working nicely is the mantra

--- src/aerial7/Aerial7.java
+++ src/aerial7/Aerial7.java
@@ -2,8 +2,10 @@ package aerial7;
 
 import java.awt.Color;
 import java.awt.image.BufferedImage;
-import java.awt.image.Raster;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 import javax.servlet.ServletException;
@@ -13,42 +15,17 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.bson.types.ObjectId;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
-import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.UUID;
-
-import com.amazonaws.AmazonClientException;
-import com.amazonaws.AmazonServiceException;
 import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.ClasspathPropertiesFileCredentialsProvider;
 import com.amazonaws.auth.PropertiesCredentials;
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.Bucket;
-import com.amazonaws.services.s3.model.GetObjectRequest;
-import com.amazonaws.services.s3.model.ListObjectsRequest;
-import com.amazonaws.services.s3.model.ObjectListing;
-import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectInputStream;
-import com.amazonaws.services.s3.model.S3ObjectSummary;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
-import com.mongodb.DBCursor;
-import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.WriteResult;
@@ -93,7 +70,7 @@ public class Aerial7 extends HttpServlet {
 		ObjectId id = new ObjectId(itemId);
 		BasicDBObject searchQuery = new BasicDBObject("_id", id);
 		BasicDBObject colourQuery = new BasicDBObject();
-		colourQuery.append("$set", new BasicDBObject("fileData", new BasicDBObject("colour", "rgb(" + color.getRed() + "," + color.getGreen() + "," + color.getBlue() + ")")));
+		colourQuery.append("$set", new BasicDBObject("fileData", new BasicDBObject("colour", color.getRed() + "," + color.getGreen() + "," + color.getBlue())));
 		WriteResult result = items.update(searchQuery, colourQuery);
 		System.out.println(result);
 	}
@@ -106,7 +83,6 @@ public class Aerial7 extends HttpServlet {
 		int blue = 0;
 		int count = 0;
 		Color color;
-		//BufferdImage scaledImg = (BufferedImage)img.getScaledInstance(200, 200, 0);
 		for(int i = 0; i < width; i++){
 			for(int j = 0; j < height; j++){
 				color = new Color(img.getRGB(i, j));
@@ -126,13 +102,12 @@ public class Aerial7 extends HttpServlet {
 			S3ObjectInputStream stream = item.getObjectContent();
 			int i = 0;
 			ArrayList<Integer> bytes = new ArrayList<Integer>();
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
 			while((i = stream.read()) != -1){
-				bytes.add(i);
+				baos.write(i);
 			}
-			_bytes = new byte[bytes.size()];
-			for(int x = 0; x < _bytes.length; x++){
-				_bytes[x] = bytes.get(x).byteValue();
-			}
+
+			_bytes = baos.toByteArray();
 			stream.read(_bytes);
 			stream.close();
 			return ImageIO.read(new ByteArrayInputStream(_bytes));	
