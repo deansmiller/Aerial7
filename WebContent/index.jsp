b1510308adf344cb3cad4c6d9d87ae1ce21759d0
Dean S. Miller <dean.sylvester.miller@gmail.com>
2013-06-08 23:33:19 +0100

    Service that extracts dominant colour from an image implemented

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -1,81 +1,68 @@
 <%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
-<%@ page import="com.amazonaws.*" %>
-<%@ page import="com.amazonaws.auth.*" %>
-<%@ page import="com.amazonaws.services.ec2.*" %>
-<%@ page import="com.amazonaws.services.ec2.model.*" %>
-<%@ page import="com.amazonaws.services.s3.*" %>
-<%@ page import="com.amazonaws.services.s3.model.*" %>
-<%@ page import="com.amazonaws.services.simpledb.*" %>
-<%@ page import="com.amazonaws.services.simpledb.model.*" %>
-
-<%! // Share the client objects across threads to
-    // avoid creating new clients for each web request
-    private AmazonEC2      ec2;
-    private AmazonS3        s3;
-    private AmazonSimpleDB sdb;
- %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page import="com.mongodb.DB" %>
+<%@ page import="com.mongodb.DBCollection" %>
+<%@ page import="com.mongodb.MongoClient" %>
+<%@ page import="com.mongodb.MongoClientURI" %>
+<%@ page import="com.mongodb.DBCursor" %>
+<%@ page import="com.mongodb.DBObject" %>
+<%@ page import="java.util.ArrayList" %>
+<%@ page import="java.util.Collections" %>
 
 <%
-    /*
-     * AWS Elastic Beanstalk checks your application's health by periodically
-     * sending an HTTP HEAD request to a resource in your application. By
-     * default, this is the root or default resource in your application,
-     * but can be configured for each environment.
-     *
-     * Here, we report success as long as the app server is up, but skip
-     * generating the whole page since this is a HEAD request only. You
-     * can employ more sophisticated health checks in your application.
-     */
-    if (request.getMethod().equals("HEAD")) return;
-%>
 
-<%
-    if (ec2 == null) {
-        AWSCredentials credentials = new PropertiesCredentials(
-            getClass().getClassLoader().getResourceAsStream("AwsCredentials.properties"));
-        ec2 = new AmazonEC2Client(credentials);
-        s3  = new AmazonS3Client(credentials);
-        sdb = new AmazonSimpleDBClient(credentials);
-    }
+	MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://nodejitsu:7b9b1cc97d6ad36ad901b426d425e436@linus.mongohq.com:10063/nodejitsudb6132056633"));
+	DB db = mongoClient.getDB("nodejitsudb6132056633");
+	DBCollection jobs = db.getCollection("aerial7jobs");
+	DBCursor cursor = jobs.find();
+	ArrayList<DBObject> jobObjects = new ArrayList<DBObject>();
+	while(cursor.hasNext()){
+		jobObjects.add(cursor.next());
+	}
+	Collections.reverse(jobObjects);
 %>
 
 <!DOCTYPE html>
 <html>
-<head>
-    <meta http-equiv="Content-type" content="text/html; charset=utf-8">
-    <title>Hello AWS Web World!</title>
-    <link rel="stylesheet" href="styles/styles.css" type="text/css" media="screen">
-</head>
-<body>
-    <div id="content" class="container">
-        <div class="section grid grid5 s3">
-            <h2>Amazon S3 Buckets:</h2>
-            <ul>
-            <% for (Bucket bucket : s3.listBuckets()) { %>
-               <li> <%= bucket.getName() %> </li>
-            <% } %>
-            </ul>
-        </div>
 
-        <div class="section grid grid5 sdb">
-            <h2>Amazon SimpleDB Domains:</h2>
-            <ul>
-            <% for (String domainName : sdb.listDomains().getDomainNames()) { %>
-               <li> <%= domainName %></li>
-            <% } %>
-            </ul>
-        </div>
-
-        <div class="section grid grid5 gridlast ec2">
-            <h2>Amazon EC2 Instances:</h2>
-            <ul>
-            <% for (Reservation reservation : ec2.describeInstances().getReservations()) { %>
-                <% for (Instance instance : reservation.getInstances()) { %>
-                   <li> <%= instance.getInstanceId() %></li>
-                <% } %>
-            <% } %>
-            </ul>
-        </div>
-    </div>
-</body>
+	<head>
+	    <meta http-equiv="Content-type" content="text/html; charset=utf-8">
+	    <title>Aerial7 Digital Media Processor</title>
+	    <link rel="stylesheet" href="styles/bootstrap.min.css" type="text/css" media="screen">
+	    <link rel="stylesheet" href="styles/styles.css" type="text/css" media="screen">
+	</head>
+	
+	<body>
+	    <div class="container">
+	    
+			<div class="page-header">
+				<h1>Aerial7 <small>Digital Media Processor</small></h1>
+			</div>
+			
+			<div class="job-container">
+				<table class="table">
+					<thead>
+						<tr>
+							<th>Item ID</th>
+							<th>Filename</th>
+							<th>Date Created</th>
+						</tr>
+					</thead>
+					
+					<tbody>
+						<% for(int i = 0; i < jobObjects.size(); i++){ %>
+						<tr>
+							<td class="itemId"><%= jobObjects.get(i).get("itemId") %></td>
+							<td class="filename"><%= jobObjects.get(i).get("filename") %></td>
+							<td class="date"><%= jobObjects.get(i).get("date") %></td>
+						<%} %>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			
+	    </div>
+	</body>
+	
+	
 </html>
--- src/aerial7/Aerial7.java
+++ src/aerial7/Aerial7.java
@@ -3,9 +3,8 @@ package aerial7;
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Calendar;
 
 import javax.imageio.ImageIO;
 import javax.servlet.ServletException;
@@ -13,6 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.io.IOUtils;
 import org.bson.types.ObjectId;
 
 import com.amazonaws.auth.AWSCredentials;
@@ -37,10 +37,11 @@ import com.mongodb.WriteResult;
 public class Aerial7 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private MongoClient mongoClient;
-	private DBCollection items;
+	private DBCollection items, jobs;
 	private AmazonS3 s3;
 	private final String BUCKET = "dam-content-bucket";
 	private final String CONTENT_DIR = "dam-content/";
+	private Calendar calendar;
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -54,6 +55,8 @@ public class Aerial7 extends HttpServlet {
 			mongoClient = new MongoClient(new MongoClientURI("mongodb://nodejitsu:7b9b1cc97d6ad36ad901b426d425e436@linus.mongohq.com:10063/nodejitsudb6132056633"));
 			DB db = mongoClient.getDB("nodejitsudb6132056633");
 			items = db.getCollection("assetitems");
+			jobs = db.getCollection("aerial7jobs");
+			calendar = Calendar.getInstance();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -63,26 +66,34 @@ public class Aerial7 extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		response.getWriter().write("Testing..");
 	}
 	
-	private void updateItem(String itemId, Color color){
+	private WriteResult updateItem(String itemId, float[] colour){
 		ObjectId id = new ObjectId(itemId);
 		BasicDBObject searchQuery = new BasicDBObject("_id", id);
 		BasicDBObject colourQuery = new BasicDBObject();
-		colourQuery.append("$set", new BasicDBObject("fileData", new BasicDBObject("colour", color.getRed() + "," + color.getGreen() + "," + color.getBlue())));
+		colourQuery.append("$set", new BasicDBObject("fileData", new BasicDBObject("colour", colour[0] + "," + colour[1] + "," + colour[2])));
 		WriteResult result = items.update(searchQuery, colourQuery);
-		System.out.println(result);
+		System.out.println(id + " -> " + result);
+		return result;
 	}
 	
-	private Color processImage(BufferedImage img){
+	private void createJobRecord(String itemId, String filename){
+		BasicDBObject job = new BasicDBObject();
+		job.put("itemId", itemId);
+		job.put("filename", filename);
+		job.put("date", calendar.getTime());
+		jobs.insert(job);
+	}
+	
+	private float[] processImage(BufferedImage img){
 		int width = img.getWidth();
 		int height = img.getHeight();
-		int red = 0;
-		int green = 0;
-		int blue = 0;
-		int count = 0;
+		int red = 0; int green = 0; int blue = 0; int count = 0;
 		Color color;
+		//should scale the image first
+		
 		for(int i = 0; i < width; i++){
 			for(int j = 0; j < height; j++){
 				color = new Color(img.getRGB(i, j));
@@ -92,46 +103,99 @@ public class Aerial7 extends HttpServlet {
 				count++;
 			}
 		}
-		return new Color(red / count, green / count, blue / count);
+		
+		float[] hsbColours = new float[3];
+		color = new Color(red / count, green / count, blue / count);
+		RGBtoHSV(red / count, green / count, blue / count, hsbColours);
+		
+		System.out.println("HSB -> " + hsbColours[0] + " " + hsbColours[1] + " " + hsbColours[2]);
+		System.out.println("RGB -> " + color);
+		
+		return hsbColours;
+	}
+	
+	//http://www.f4.fhtw-berlin.de/~barthel/ImageJ/ColorInspector/HTMLHelp/farbraumJava.htm
+	private void RGBtoHSV(float r, float g, float b, float hsl[]) {
+		
+		float var_R = ( r / 255f );                    
+		float var_G = ( g / 255f );
+		float var_B = ( b / 255f );
+		
+		float var_Min;    //Min. value of RGB
+		float var_Max;    //Max. value of RGB
+		float del_Max;    //Delta RGB value
+						 
+		if (var_R > var_G) 
+			{ var_Min = var_G; var_Max = var_R; }
+		else 
+			{ var_Min = var_R; var_Max = var_G; }
+
+		if (var_B > var_Max) var_Max = var_B;
+		if (var_B < var_Min) var_Min = var_B;
+
+		del_Max = var_Max - var_Min; 
+								 
+		float H = 0, S, L;
+		L = ( var_Max + var_Min ) / 2f;
+	
+		if ( del_Max == 0 ) { H = 0; S = 0; } // gray
+		else {                                //Chroma
+			if ( L < 0.5 ) 
+				S = del_Max / ( var_Max + var_Min );
+			else           
+				S = del_Max / ( 2 - var_Max - var_Min );
+	
+			float del_R = ( ( ( var_Max - var_R ) / 6f ) + ( del_Max / 2f ) ) / del_Max;
+			float del_G = ( ( ( var_Max - var_G ) / 6f ) + ( del_Max / 2f ) ) / del_Max;
+			float del_B = ( ( ( var_Max - var_B ) / 6f ) + ( del_Max / 2f ) ) / del_Max;
+	
+			if ( var_R == var_Max ) 
+				H = del_B - del_G;
+			else if ( var_G == var_Max ) 
+				H = ( 1 / 3f ) + del_R - del_B;
+			else if ( var_B == var_Max ) 
+				H = ( 2 / 3f ) + del_G - del_R;
+			if ( H < 0 ) H += 1;
+			if ( H > 1 ) H -= 1;
+		}
+		hsl[0] = (int)(360*H);
+		hsl[1] = (int)(S*100);
+		hsl[2] = (int)(L*100);
+		
 	}
 	
 	
 	private BufferedImage convertToImage(S3Object item){
-		byte[] _bytes = null;
 		try {
 			S3ObjectInputStream stream = item.getObjectContent();
-			int i = 0;
-			ArrayList<Integer> bytes = new ArrayList<Integer>();
-			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			while((i = stream.read()) != -1){
-				baos.write(i);
-			}
-
-			_bytes = baos.toByteArray();
-			stream.read(_bytes);
-			stream.close();
-			return ImageIO.read(new ByteArrayInputStream(_bytes));	
+			byte[] bytes = IOUtils.toByteArray(stream);
+			return ImageIO.read(new ByteArrayInputStream(bytes));	
 		} catch(IOException e){
 			System.out.println(e);
+			return null;
 		}
-		
-		return null;
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		String cmd = request.getParameter("cmd");
 		String itemId = request.getParameter("item");
 		String filename = request.getParameter("filename");
 		
 		if(cmd.equals("PROCESS")){
-			S3Object item = s3.getObject(BUCKET, CONTENT_DIR + itemId + "/" + filename);
+			S3Object item = getItem(itemId, filename);
 			BufferedImage convertedImg = convertToImage(item);
-			Color colour = processImage(convertedImg);
+			float[] colour = processImage(convertedImg);
 			updateItem(itemId, colour);
+			createJobRecord(itemId, filename);
 		}
+	}
+	
+	private S3Object getItem(String itemId, String filename) {
+		return s3.getObject(BUCKET, CONTENT_DIR + itemId + "/" + filename);
 	}
 
 }
